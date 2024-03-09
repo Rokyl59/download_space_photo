2ccf1c55abe34a5bf644acf70f261f2e92f3a7b9
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-03-09 12:54:39 +0500

    Post a photo in the Telegram channel by time

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,16 +1,31 @@
-import telegram
-from dotenv import load_dotenv
 import os
+import time
+import random
+import argparse
+from dotenv import load_dotenv
+import telegram
 
 
-load_dotenv()
+def publish_photos(directory, interval_hours):
+    photos = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    
+    random.shuffle(photos)
 
-token = os.getenv('TELEGRAM_TOKEN_API')
-bot = telegram.Bot(token=token)
+    while True:
+        for photo in photos:
+            photo_path = os.path.join(directory, photo)
+            bot.send_document(chat_id='@teeestbot_channel', document=open(photo_path, 'rb'))
+            time.sleep(interval_hours * 6)
+        
+        random.shuffle(photos)
 
 
-print(bot.get_me())
-
-bot.send_message(text='Hi John!', chat_id='@teeestbot_channel')
-
-bot.send_document(chat_id='@teeestbot_channel', document=open('images/nasa_0.jpg', 'rb'))
+if __name__ == "__main__":
+    load_dotenv()
+    token = os.getenv('TELEGRAM_TOKEN_API')
+    bot = telegram.Bot(token=token)
+    parser = argparse.ArgumentParser(description='Publish photos to Telegram channel.')
+    parser.add_argument('--directory', type=str, help='Directory with photos to publish')
+    parser.add_argument('--interval', type=int, default=4, help='Interval between posts in hours')
+    args = parser.parse_args()
+    publish_photos(args.directory, args.interval)
