6dcad73fe8772a736d39c00390c4d0b691adb8cf
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-03-11 21:53:23 +0500

    Уточнил название переменной

--- fetch_epic_images.py
+++ fetch_epic_images.py
@@ -12,15 +12,12 @@ def fetch_recent_epic_image_metadata(api_key, count=10):
     return response.json()[:count]
 
 
-def download_and_save_epic_images(api_key, epic_images_metadata):
+def download_and_save_epic_image(api_key, image_info, index):
     date_parts = image_info['date'].split(' ')[0].split('-')
     image_filename = image_info['image']
-
     url_template = f'https://api.nasa.gov/EPIC/archive/natural/{date_parts[0]}/{date_parts[1]}/{date_parts[2]}/png/{image_filename}.png'
-
     ext = get_file_extension(url_template)
     filename = f'epic_{index}{ext}'
-
     fetch_image(url_template, 'images', filename, params={'api_key': api_key})
 
 
@@ -28,7 +25,5 @@ if __name__ == '__main__':
     load_dotenv()
     api_key = os.getenv('NASA_API_KEY')
     epic_images_metadata = fetch_recent_epic_image_metadata(api_key)
-
     for index, image_info in enumerate(epic_images_metadata):
-        download_and_save_epic_images(api_key, epic_images_metadata)
-        
+        download_and_save_epic_image(api_key, image_info, index)
