1402e84aebd3172267b2acbbb0495698d32e0da2
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-03-11 16:43:59 +0500

    Упростил создание папки

--- common_functions.py
+++ common_functions.py
@@ -12,8 +12,7 @@ def get_file_extension(url):
 
 
 def fetch_image(url, path, filename):
-    if not os.path.exists(path):
-        os.makedirs(path)
+    os.makedirs(path, exist_ok=True)
     response = requests.get(url)
     response.raise_for_status()
     content = response.content
